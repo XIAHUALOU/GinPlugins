99fce07071a4aba46e6d6737a9f6864ba2520b31
夏华楼 <390306467@qq.com>
2021-04-01 16:33:07 +0800

    项目开始

---
+++ AddHeader.go
@@ -0,0 +1,13 @@
+package GinPlugins
+
+import "net/http"
+
+type AddHeader struct{}
+
+func New() http.Handler {
+	return &AddHeader{}
+}
+
+func (this *AddHeader) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
+	writer.Header().Add("name", "xiahualou")
+}
